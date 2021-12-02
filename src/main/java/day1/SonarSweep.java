6e925442217d6c12fc26409944190c7d32c3ad90
Michal Baran <michal.baran.dev@gmail.com>
2021-12-02 01:45:26 +0100

    day 1 stage 2

--- src/main/java/day1/SonarSweep.java
+++ src/main/java/day1/SonarSweep.java
@@ -21,7 +21,6 @@ public class SonarSweep {
     }
 
     public static int countIncreases(List<Integer> measurements) {
-        System.out.println(measurements.size());
         int increases = 0;
         for (int i = 0; i < measurements.size() - 1; i++) {
             if (measurements.get(i) < measurements.get(i + 1)) {
@@ -31,9 +30,22 @@ public class SonarSweep {
         return increases;
     }
 
+    public static int slidingWindow(List<Integer> measurements) {
+        int increases = 0;
+        for (int i = 0; i < measurements.size() - 3; i++) {
+            int common = measurements.get(i + 1) + measurements.get(i + 2);
+            int window1 = measurements.get(i) + common;
+            int window2 = common + measurements.get(i + 3);
+            if (window1 < window2) {
+                increases++;
+            }
+        }
+        return increases;
+    }
+
     public static void main(String[] args) {
         List<Integer> measurements = generateListFromFile("D:\\4. PROGRAMOWANIE\\Java\\Projekty\\AdventOfCode2021\\src\\main\\java\\day1\\input.txt");
-        int increases = countIncreases(measurements);
-        System.out.println("Wynik to " + increases);
+        System.out.println(countIncreases(measurements));
+        System.out.println(slidingWindow(measurements));
     }
 }
